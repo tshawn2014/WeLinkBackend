fbc9f057b5072bf55ea2051eeddfc64d90fffddb
Shihan <tshawn2014@gmail.com>
2020-10-27 23:00:49 -0700

    minor change

--- WeLinkAPI/api/views.py
+++ WeLinkAPI/api/views.py
@@ -5,11 +5,12 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .util import login
 
 def index(request):
-    return HttpResponse("Hello, world! You can post your life here!"+request.session['oauth_struct'])
+    return HttpResponse("Hello, world! You can post your life here!<br>"+request.session['oauth_struct'])
 
 def callback(request):
     return login.redirect_back(request)
 
 def login_test(request):
+    del request.session['oauth_struct']
     res = login.request_auth(request)
     return HttpResponseRedirect(str(res))
